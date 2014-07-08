6c0aa51c7e5066ef522fb0b35e9101281df86094
Wenxi Jin <wjin@vitesse.com>
2014-07-08 15:29:30 +0200

    VTSS: Vcoreiii - Added uio platform driver

--- arch/mips/vcoreiii/vcoreiii_platform.c
+++ arch/mips/vcoreiii/vcoreiii_platform.c
@@ -27,19 +27,19 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/serial_8250.h>
+#include <linux/uio_driver.h>
 
 #include <asm/mach-vcoreiii/hardware.h>
 #include <asm/mach-vcoreiii/i2c.h>
 
-#define CBUS_UART_FLAGS (UPF_FIXED_TYPE | UPF_SKIP_TEST | UPF_IOREMAP)
-
+/* 8250 */
 static struct plat_serial8250_port uart8250_vcoreiii_data[] = {
 	{
 		.mapbase	= 0x70100000,
 		.irq		= UART_IRQ,
 		.uartclk	= VCOREIII_AHB_CLOCK,
 		.iotype		= UPIO_MEM32,
-		.flags		= CBUS_UART_FLAGS,
+		.flags		= (UPF_BOOT_AUTOCONF | UPF_SKIP_TEST | UPF_IOREMAP),
                 .type           = PORT_16550A,
 		.regshift	= 2,
 	},
@@ -54,28 +54,58 @@ static struct platform_device uart8250_device = {
 	},
 };
 
-static int __init uart8250_init(void)
-{
-    return platform_device_register(&uart8250_device);
-}
-module_init(uart8250_init);
+/* Switch UIO */
+static struct uio_info switch_platform_data = {
+    .name = "vcoreiii_switch",
+    .version = "0",
+    .irq = INT1_IRQ,    // Alternate IRQ. Application must remap IRQ destination
+};
 
+static struct resource switch_resources[] = {
+    [0] = {
+        .name   = "origin1_2",
+        .start  = VTSS_IO_ORIGIN1_OFFSET,
+        .end    = VTSS_IO_ORIGIN2_OFFSET + VTSS_IO_ORIGIN2_SIZE - 1,
+        .flags  = IORESOURCE_MEM,
+    },
+};
+
+static struct platform_device switch_device = {
+    .name           = "uio_pdrv_genirq",
+    .id             = 0,
+    .dev = {
+        .platform_data  = &switch_platform_data,
+    },
+    .resource       = switch_resources,
+    .num_resources  = ARRAY_SIZE(switch_resources),
+};
+
+  
+/* I2C */
 static struct vcoreiii_i2c_platform_data i2c_data = {
     .fast_mode = 0,             /* 100 kbit/s */
 };
 
-static int __init vcoreiii_i2c_init(void)
+static struct platform_device i2c_device = {
+    .name                   = "i2c_vcoreiii",
+    .id                     = -1,
+    .dev = {
+            .platform_data = &i2c_data,
+    },
+};
+
+static struct platform_device *target_devices[] __initdata = {
+    &uart8250_device,
+    &switch_device,
+    &i2c_device,
+};
+
+static int __init target_device_init(void)
 {
-    int ret;
-    struct platform_device *dev = platform_device_register_simple("i2c_vcoreiii", -1, NULL, 0);
-    if(IS_ERR(dev))
-        return PTR_ERR(dev);
-    if((ret = platform_device_add_data(dev, &i2c_data, sizeof(struct vcoreiii_i2c_platform_data))))
-        platform_device_unregister(dev);
-    return ret;
+    return platform_add_devices(target_devices, ARRAY_SIZE(target_devices));
 }
-device_initcall(vcoreiii_i2c_init);
+module_init(target_device_init);
 
 MODULE_AUTHOR("Lars Povlsen <lpovlsen@vitesse.com>");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("8250 UART probe driver for the VCore-III UART");
+MODULE_DESCRIPTION("Platform drivers for Vcoreiii platform");
